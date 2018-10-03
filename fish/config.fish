8a9dad505aa5b6a786928e82b2273c3394f8e6bd
Mark Tozzi <mark.tozzi@gmail.com>
2018-10-03 14:16:44 -0400

    Java home paths for elastic development

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,11 @@ eval (python3 -m virtualfish)
 fundle plugin 'tuvistavie/fish-ssh-agent'
 
 fundle init
+
+set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk-11.jdk/Contents/Home
+set -x RUNTIME_JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home
+
+set -x JAVA10_HOME /Library/Java/JavaVirtualMachines/jdk-10.0.2.jdk/Contents/Home/
+set -x JAVA11_HOME /Library/Java/JavaVirtualMachines/jdk-11.jdk/Contents/Home
+set -x JAVA7_HOME /Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home
+set -x JAVA8_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home
