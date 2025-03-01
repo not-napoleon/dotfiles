edfc333733c5736cd755d05380b34fb42fe91120
Mark Tozzi <mark.tozzi@gmail.com>
2025-02-28 21:15:54 -0500

    this works

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,43 @@
--- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
--- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
-local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+-- vim: ts=2 sts=2 sw=2 et
+-- Bootstrap lazy.nvim
+-- this data path is ~/.local/share/nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
--- validate that lazy is available
-if not pcall(require, "lazy") then
-  -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
-  vim.fn.getchar()
-  vim.cmd.quit()
-end
+local opt = vim.opt
 
-require "lazy_setup"
-require "polish"
+opt.cursorline = true
+opt.number = true
+
+opt.splitbelow = true
+opt.splitright = true
+
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.shiftround = true
+opt.tabstop = 4
+
+require("lazy").setup({
+    -- theme
+  { "catppuccin/nvim", lazy = true, name = "catppuccin", priority=1000 },
+
+   -- devicons
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+
+})
+
+-- set colour scheme
+vim.cmd.colorscheme "catppuccin-macchiato"
