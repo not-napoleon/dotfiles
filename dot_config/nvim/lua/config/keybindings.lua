48060d2c25ecc826a7303aec26980d0e5ef35b58
Mark Tozzi <mark.tozzi@gmail.com>
2026-05-08 20:54:53 -0400

    whitespace

--- dot_config/nvim/lua/config/keybindings.lua
+++ dot_config/nvim/lua/config/keybindings.lua
@@ -23,3 +23,4 @@ vim.keymap.set({'n', 'o', 'v'}, 'f', '<Plug>(easymotion-bd-fl)')
 vim.keymap.set({'n', 'o', 'v'}, 'F', '<Plug>(easymotion-bd-f)')
 vim.keymap.set({'n', 'o', 'v'}, 't', '<Plug>(easymotion-bd-tl)')
 vim.keymap.set({'n', 'o', 'v'}, 'T', '<Plug>(easymotion-bd-t)')
+
